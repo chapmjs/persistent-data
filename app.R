f096098e7b8b7e97ced076838912a9f0b1c92e11
John Chapman <684704+chapmjs@users.noreply.github.com>
2019-06-04 13:50:01 -0400

    put datatable on top of input form

--- app.R
+++ app.R
@@ -59,12 +59,12 @@ loadData <- function() {
 # Shiny app with 3 fields that the user can submit data for
 shinyApp(
   ui = fluidPage(
+    DT::dataTableOutput("responses", width = 300), tags$hr(),
     textInput("name", "Name", ""),
     checkboxInput("used_shiny", "I've built a Shiny app in R before", FALSE),
     sliderInput("r_num_years", "Number of years using R",
                 0, 25, 2, ticks = FALSE),
-    actionButton("submit", "Submit"),
-    DT::dataTableOutput("responses", width = 300), tags$hr()
+    actionButton("submit", "Submit")
     
   ),
   server = function(input, output, session) {
